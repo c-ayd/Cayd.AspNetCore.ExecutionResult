24fa41a4dce7b4952bd50967731c20ab3b5c79e5
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 18:05:23 +0200

    add execution client error result: length required

---
+++ src/Cayd.AspNetCore.ExecutionResult/ClientError/ExecLengthRequired.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.ClientError
+{
+    /// <summary>
+    /// Defines a failed execution result with the status code of 411.
+    /// </summary>
+    public class ExecLengthRequired : ExecError
+    {
+        private const int errorCode = (int)HttpStatusCode.LengthRequired;
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 411.
+        /// </summary>
+        /// <param name="details">The details of the errors of the exeucution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecLengthRequired(ICollection<ExecErrorDetail> details, object? metadata = null) : base(errorCode, details, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 411.
+        /// </summary>
+        /// <param name="detail">The detail of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecLengthRequired(ExecErrorDetail detail, object? metadata = null) : base(errorCode, detail, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 411.
+        /// </summary>
+        /// <param name="message">The message of the error of the execution.</param>
+        /// <param name="messageCode">The message code of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecLengthRequired(string? message = null, string? messageCode = null, object? metadata = null) : base(errorCode, message, messageCode, metadata)
+        {
+        }
+    }
+}
