4cee15c48eb11949e17cd0cf3eed3caf090db04d
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 18:10:10 +0200

    add execution client error result: unsupported media type

---
+++ src/Cayd.AspNetCore.ExecutionResult/ClientError/ExecUnsupportedMediaType.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.ClientError
+{
+    /// <summary>
+    /// Defines a failed execution result with the status code of 415.
+    /// </summary>
+    public class ExecUnsupportedMediaType : ExecError
+    {
+        private const int errorCode = (int)HttpStatusCode.UnsupportedMediaType;
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 415.
+        /// </summary>
+        /// <param name="details">The details of the errors of the exeucution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecUnsupportedMediaType(ICollection<ExecErrorDetail> details, object? metadata = null) : base(errorCode, details, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 415.
+        /// </summary>
+        /// <param name="detail">The detail of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecUnsupportedMediaType(ExecErrorDetail detail, object? metadata = null) : base(errorCode, detail, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 415.
+        /// </summary>
+        /// <param name="message">The message of the error of the execution.</param>
+        /// <param name="messageCode">The message code of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecUnsupportedMediaType(string? message = null, string? messageCode = null, object? metadata = null) : base(errorCode, message, messageCode, metadata)
+        {
+        }
+    }
+}
