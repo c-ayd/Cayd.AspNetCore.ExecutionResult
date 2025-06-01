c717bf2bd7bbbfd075d33a3b957672b0208fb5a7
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 18:15:57 +0200

    add execution client error result: unprocessable content

---
+++ src/Cayd.AspNetCore.ExecutionResult/ClientError/ExecUnprocessableContent.cs
@@ -0,0 +1,43 @@
+﻿#if NET8_0_OR_GREATER
+using System.Collections.Generic;
+using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.ClientError
+{
+    /// <summary>
+    /// Defines a failed execution result with the status code of 422.
+    /// </summary>
+    public class ExecUnprocessableContent : ExecError
+    {
+        private const int errorCode = (int)HttpStatusCode.UnprocessableContent;
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 422.
+        /// </summary>
+        /// <param name="details">The details of the errors of the exeucution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecUnprocessableContent(ICollection<ExecErrorDetail> details, object? metadata = null) : base(errorCode, details, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 422.
+        /// </summary>
+        /// <param name="detail">The detail of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecUnprocessableContent(ExecErrorDetail detail, object? metadata = null) : base(errorCode, detail, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 422.
+        /// </summary>
+        /// <param name="message">The message of the error of the execution.</param>
+        /// <param name="messageCode">The message code of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecUnprocessableContent(string? message = null, string? messageCode = null, object? metadata = null) : base(errorCode, message, messageCode, metadata)
+        {
+        }
+    }
+}
+#endif
