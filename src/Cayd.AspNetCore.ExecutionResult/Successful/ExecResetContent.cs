bb070bc94214a35c89e981934faf0e8bd4586952
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 17:25:13 +0200

    add execution success result: reset content

---
+++ src/Cayd.AspNetCore.ExecutionResult/Successful/ExecResetContent.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.Successful
+{
+    /// <summary>
+    /// Defines a successful execution result with the status code of 205.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value returned by the execution.</typeparam>
+    public class ExecResetContent<TValue> : ExecSuccess<TValue>
+    {
+        private const int successCode = (int)HttpStatusCode.ResetContent;
+
+        /// <summary>
+        /// Creates a successful execution result with the status code of 205.
+        /// </summary>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecResetContent(object? metadata = null) : base(successCode, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a successful execution result with the status code of 205.
+        /// </summary>
+        /// <param name="value">The value returned by the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecResetContent(TValue? value, object? metadata = null) : base(successCode, value, metadata)
+        {
+        }
+    }
+}
