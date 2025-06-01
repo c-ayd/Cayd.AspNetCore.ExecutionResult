e4e926ad64d1da781a22479778073f0f9b883973
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 17:22:51 +0200

    add execution success result: non-authoritative information

---
+++ src/Cayd.AspNetCore.ExecutionResult/Successful/ExecNonAuthoritativeInformation.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.Successful
+{
+    /// <summary>
+    /// Defines a successful execution result with the status code of 203.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value returned by the execution.</typeparam>
+    public class ExecNonAuthoritativeInformation<TValue> : ExecSuccess<TValue>
+    {
+        private const int successCode = (int)HttpStatusCode.NonAuthoritativeInformation;
+
+        /// <summary>
+        /// Creates a successful execution result with the status code of 203.
+        /// </summary>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecNonAuthoritativeInformation(object? metadata = null) : base(successCode, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a successful execution result with the status code of 203.
+        /// </summary>
+        /// <param name="value">The value returned by the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecNonAuthoritativeInformation(TValue? value, object? metadata = null) : base(successCode, value, metadata)
+        {
+        }
+    }
+}
