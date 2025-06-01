958b7fbd2163b80eeb8db6edf59c82a364825396
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 18:22:51 +0200

    add execution client error result: precondition required

---
+++ src/Cayd.AspNetCore.ExecutionResult/ClientError/ExecPreconditionRequired.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.ClientError
+{
+    /// <summary>
+    /// Defines a failed execution result with the status code of 428.
+    /// </summary>
+    public class ExecPreconditionRequired : ExecError
+    {
+        private const int errorCode = (int)HttpStatusCode.PreconditionRequired;
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 428.
+        /// </summary>
+        /// <param name="details">The details of the errors of the exeucution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecPreconditionRequired(ICollection<ExecErrorDetail> details, object? metadata = null) : base(errorCode, details, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 428.
+        /// </summary>
+        /// <param name="detail">The detail of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecPreconditionRequired(ExecErrorDetail detail, object? metadata = null) : base(errorCode, detail, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 428.
+        /// </summary>
+        /// <param name="message">The message of the error of the execution.</param>
+        /// <param name="messageCode">The message code of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecPreconditionRequired(string? message = null, string? messageCode = null, object? metadata = null) : base(errorCode, message, messageCode, metadata)
+        {
+        }
+    }
+}
