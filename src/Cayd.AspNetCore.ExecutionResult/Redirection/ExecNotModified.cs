d47cb6c321ef06c306044bd5276687bbf2b4ac4c
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 17:40:00 +0200

    add execution redirection result: not modified

---
+++ src/Cayd.AspNetCore.ExecutionResult/Redirection/ExecNotModified.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.Redirection
+{
+    /// <summary>
+    /// Defines a redirection execution result with the status code of 304.
+    /// </summary>
+    public class ExecNotModified : ExecRedirection
+    {
+        private const int redirectionCode = (int)HttpStatusCode.NotModified;
+
+        /// <summary>
+        /// Creates a redirection execution result with the status code of 304.
+        /// </summary>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecNotModified(object? metadata = null) : base(redirectionCode, metadata)
+        {
+        }
+    }
+}
