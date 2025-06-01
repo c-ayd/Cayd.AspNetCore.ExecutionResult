3f59d52980c30f012a4f559b1acbb45919485eea
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 18:37:27 +0200

    add execution server error result: loop detected

---
+++ src/Cayd.AspNetCore.ExecutionResult/ServerError/ExecLoopDetected.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.ServerError
+{
+    /// <summary>
+    /// Defines a failed execution result with the status code of 508.
+    /// </summary>
+    public class ExecLoopDetected : ExecError
+    {
+        private const int errorCode = (int)HttpStatusCode.LoopDetected;
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 508.
+        /// </summary>
+        /// <param name="details">The details of the errors of the exeucution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecLoopDetected(ICollection<ExecErrorDetail> details, object? metadata = null) : base(errorCode, details, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 508.
+        /// </summary>
+        /// <param name="detail">The detail of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecLoopDetected(ExecErrorDetail detail, object? metadata = null) : base(errorCode, detail, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 508.
+        /// </summary>
+        /// <param name="message">The message of the error of the execution.</param>
+        /// <param name="messageCode">The message code of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecLoopDetected(string? message = null, string? messageCode = null, object? metadata = null) : base(errorCode, message, messageCode, metadata)
+        {
+        }
+    }
+}
