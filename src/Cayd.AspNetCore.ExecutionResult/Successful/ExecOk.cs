ef1862b085d01f5085b40e5290acd5c4c2051880
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 16:58:55 +0200

    add execution success result: ok

---
+++ src/Cayd.AspNetCore.ExecutionResult/Successful/ExecOk.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.Successful
+{
+    /// <summary>
+    /// Defines a successful execution result with the status code of 200.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value returned by the execution.</typeparam>
+    public class ExecOk<TValue> : ExecSuccess<TValue>
+    {
+        private const int successCode = (int)HttpStatusCode.OK;
+
+        /// <summary>
+        /// Creates a successful execution result with the status code of 200.
+        /// </summary>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecOk(object? metadata = null) : base(successCode, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a successful execution result with the status code of 200.
+        /// </summary>
+        /// <param name="value">The value returned by the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecOk(TValue? value, object? metadata = null) : base(successCode, value, metadata)
+        {
+        }
+    }
+}
