c09fa0141266afa779d5c6706f2a89ca3374e3a0
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 17:24:01 +0200

    add execution success result: no content

---
+++ src/Cayd.AspNetCore.ExecutionResult/Successful/ExecNoContent.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.Successful
+{
+    /// <summary>
+    /// Defines a successful execution result with the status code of 204.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value returned by the execution.</typeparam>
+    public class ExecNoContent<TValue> : ExecSuccess<TValue>
+    {
+        private const int successCode = (int)HttpStatusCode.NoContent;
+
+        /// <summary>
+        /// Creates a successful execution result with the status code of 204.
+        /// </summary>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecNoContent(object? metadata = null) : base(successCode, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a successful execution result with the status code of 204.
+        /// </summary>
+        /// <param name="value">The value returned by the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecNoContent(TValue? value, object? metadata = null) : base(successCode, value, metadata)
+        {
+        }
+    }
+}
