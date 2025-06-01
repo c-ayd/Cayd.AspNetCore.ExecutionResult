d72397c303681c835c423d1abb011069fcc79356
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 17:36:11 +0200

    add execution redirection result: multiple choices

---
+++ src/Cayd.AspNetCore.ExecutionResult/Redirection/ExecMultipleChoices.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.Redirection
+{
+    /// <summary>
+    /// Defines a redirection execution result with the status code of 300.
+    /// </summary>
+    public class ExecMultipleChoices : ExecRedirection
+    {
+        private const int redirectionCode = (int)HttpStatusCode.MultipleChoices;
+
+        /// <summary>
+        /// Creates a redirection execution result with the status code of 300.
+        /// </summary>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecMultipleChoices(object? metadata = null) : base(redirectionCode, metadata)
+        {
+        }
+    }
+}
