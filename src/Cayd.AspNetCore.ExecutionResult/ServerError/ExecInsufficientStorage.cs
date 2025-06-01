630d17d6fa51fbac8c2c917de3668865fd7e6c96
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 18:36:37 +0200

    add execution server error result: insufficient storage

---
+++ src/Cayd.AspNetCore.ExecutionResult/ServerError/ExecInsufficientStorage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.ServerError
+{
+    /// <summary>
+    /// Defines a failed execution result with the status code of 507.
+    /// </summary>
+    public class ExecInsufficientStorage : ExecError
+    {
+        private const int errorCode = (int)HttpStatusCode.InsufficientStorage;
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 507.
+        /// </summary>
+        /// <param name="details">The details of the errors of the exeucution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecInsufficientStorage(ICollection<ExecErrorDetail> details, object? metadata = null) : base(errorCode, details, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 507.
+        /// </summary>
+        /// <param name="detail">The detail of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecInsufficientStorage(ExecErrorDetail detail, object? metadata = null) : base(errorCode, detail, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 507.
+        /// </summary>
+        /// <param name="message">The message of the error of the execution.</param>
+        /// <param name="messageCode">The message code of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecInsufficientStorage(string? message = null, string? messageCode = null, object? metadata = null) : base(errorCode, message, messageCode, metadata)
+        {
+        }
+    }
+}
