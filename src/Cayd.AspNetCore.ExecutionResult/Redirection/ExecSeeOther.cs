ac5f1b920ac7ea39ff1dcfd7e8e5bf328a92d8e0
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 17:38:53 +0200

    add execution redirection result: see other

---
+++ src/Cayd.AspNetCore.ExecutionResult/Redirection/ExecSeeOther.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.Redirection
+{
+    /// <summary>
+    /// Defines a redirection execution result with the status code of 303.
+    /// </summary>
+    public class ExecSeeOther : ExecRedirection
+    {
+        private const int redirectionCode = (int)HttpStatusCode.SeeOther;
+
+        /// <summary>
+        /// Creates a redirection execution result with the status code of 303.
+        /// </summary>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecSeeOther(object? metadata = null) : base(redirectionCode, metadata)
+        {
+        }
+    }
+}
