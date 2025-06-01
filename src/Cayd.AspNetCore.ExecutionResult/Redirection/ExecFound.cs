3d210661bccd1e5030ca8094b918f79b47b74071
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 17:38:07 +0200

    add execution redirection result: found

---
+++ src/Cayd.AspNetCore.ExecutionResult/Redirection/ExecFound.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.Redirection
+{
+    /// <summary>
+    /// Defines a redirection execution result with the status code of 302.
+    /// </summary>
+    public class ExecFound : ExecRedirection
+    {
+        private const int redirectionCode = (int)HttpStatusCode.Found;
+
+        /// <summary>
+        /// Creates a redirection execution result with the status code of 302.
+        /// </summary>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecFound(object? metadata = null) : base(redirectionCode, metadata)
+        {
+        }
+    }
+}
