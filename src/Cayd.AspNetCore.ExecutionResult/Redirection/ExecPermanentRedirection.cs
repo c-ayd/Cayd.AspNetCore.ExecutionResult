0093f339941abc695014e7aba52763f43d2924d8
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 17:43:40 +0200

    add execution redirection result: permanent redirection

---
+++ src/Cayd.AspNetCore.ExecutionResult/Redirection/ExecPermanentRedirection.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.Redirection
+{
+    /// <summary>
+    /// Defines a redirection execution result with the status code of 308.
+    /// </summary>
+    public class ExecPermanentRedirection : ExecRedirection
+    {
+        private const int redirectionCode = (int)HttpStatusCode.PermanentRedirect;
+
+        /// <summary>
+        /// Creates a redirection execution result with the status code of 308.
+        /// </summary>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecPermanentRedirection(object? metadata = null) : base(redirectionCode, metadata)
+        {
+        }
+    }
+}
