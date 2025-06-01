4acd1833336eb39cb98fbead184aeb32565890b9
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 17:30:00 +0200

    add execution success result: already reported

---
+++ src/Cayd.AspNetCore.ExecutionResult/Successful/ExecAlreadyReported.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.Successful
+{
+    /// <summary>
+    /// Defines a successful execution result with the status code of 208.
+    /// </summary>
+    /// <typeparam name="TValue">The type of the value returned by the execution.</typeparam>
+    public class ExecAlreadyReported<TValue> : ExecSuccess<TValue>
+    {
+        private const int successCode = (int)HttpStatusCode.AlreadyReported;
+
+        /// <summary>
+        /// Creates a successful execution result with the status code of 208.
+        /// </summary>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecAlreadyReported(object? metadata = null) : base(successCode, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a successful execution result with the status code of 208.
+        /// </summary>
+        /// <param name="value">The value returned by the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecAlreadyReported(TValue? value, object? metadata = null) : base(successCode, value, metadata)
+        {
+        }
+    }
+}
