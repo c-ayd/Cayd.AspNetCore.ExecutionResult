08eb945ce286febfc5d123a9f5223d211c11d9c9
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 17:54:15 +0200

    add execution client error result: payment required

---
+++ src/Cayd.AspNetCore.ExecutionResult/ClientError/ExecPaymentRequired.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.ClientError
+{
+    /// <summary>
+    /// Defines a failed execution result with the status code of 402.
+    /// </summary>
+    public class ExecPaymentRequired : ExecError
+    {
+        private const int errorCode = (int)HttpStatusCode.PaymentRequired;
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 402.
+        /// </summary>
+        /// <param name="details">The details of the errors of the exeucution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecPaymentRequired(ICollection<ExecErrorDetail> details, object? metadata = null) : base(errorCode, details, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 402.
+        /// </summary>
+        /// <param name="detail">The detail of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecPaymentRequired(ExecErrorDetail detail, object? metadata = null) : base(errorCode, detail, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 402.
+        /// </summary>
+        /// <param name="message">The message of the error of the execution.</param>
+        /// <param name="messageCode">The message code of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecPaymentRequired(string? message = null, string? messageCode = null, object? metadata = null) : base(errorCode, message, messageCode, metadata)
+        {
+        }
+    }
+}
