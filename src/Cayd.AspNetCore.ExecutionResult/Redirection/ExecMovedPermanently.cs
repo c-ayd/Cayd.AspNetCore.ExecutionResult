7273ab2547e54f11cc3299eb2ab37ecc63bee695
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 17:37:09 +0200

    add execution redirection result: moved permanently

---
+++ src/Cayd.AspNetCore.ExecutionResult/Redirection/ExecMovedPermanently.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.Redirection
+{
+    /// <summary>
+    /// Defines a redirection execution result with the status code of 301.
+    /// </summary>
+    public class ExecMovedPermanently : ExecRedirection
+    {
+        private const int redirectionCode = (int)HttpStatusCode.MovedPermanently;
+
+        /// <summary>
+        /// Creates a redirection execution result with the status code of 301.
+        /// </summary>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecMovedPermanently(object? metadata = null) : base(redirectionCode, metadata)
+        {
+        }
+    }
+}
