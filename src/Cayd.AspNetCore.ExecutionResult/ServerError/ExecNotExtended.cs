c67a3d184c2ea94160650682b8ae0d05ab75a062
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 18:38:37 +0200

    add execution server error result: not extended

---
+++ src/Cayd.AspNetCore.ExecutionResult/ServerError/ExecNotExtended.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.ServerError
+{
+    /// <summary>
+    /// Defines a failed execution result with the status code of 510.
+    /// </summary>
+    public class ExecNotExtended : ExecError
+    {
+        private const int errorCode = (int)HttpStatusCode.NotExtended;
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 510.
+        /// </summary>
+        /// <param name="details">The details of the errors of the exeucution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecNotExtended(ICollection<ExecErrorDetail> details, object? metadata = null) : base(errorCode, details, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 510.
+        /// </summary>
+        /// <param name="detail">The detail of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecNotExtended(ExecErrorDetail detail, object? metadata = null) : base(errorCode, detail, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 510.
+        /// </summary>
+        /// <param name="message">The message of the error of the execution.</param>
+        /// <param name="messageCode">The message code of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecNotExtended(string? message = null, string? messageCode = null, object? metadata = null) : base(errorCode, message, messageCode, metadata)
+        {
+        }
+    }
+}
