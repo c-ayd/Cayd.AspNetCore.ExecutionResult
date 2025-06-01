5b881a3333edf2285980d85a1421360d76cf545c
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 18:24:13 +0200

    add execution client error result: too many requests

---
+++ src/Cayd.AspNetCore.ExecutionResult/ClientError/ExecTooManyRequests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.ClientError
+{
+    /// <summary>
+    /// Defines a failed execution result with the status code of 429.
+    /// </summary>
+    public class ExecTooManyRequests : ExecError
+    {
+        private const int errorCode = (int)HttpStatusCode.TooManyRequests;
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 429.
+        /// </summary>
+        /// <param name="details">The details of the errors of the exeucution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecTooManyRequests(ICollection<ExecErrorDetail> details, object? metadata = null) : base(errorCode, details, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 429.
+        /// </summary>
+        /// <param name="detail">The detail of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecTooManyRequests(ExecErrorDetail detail, object? metadata = null) : base(errorCode, detail, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 429.
+        /// </summary>
+        /// <param name="message">The message of the error of the execution.</param>
+        /// <param name="messageCode">The message code of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecTooManyRequests(string? message = null, string? messageCode = null, object? metadata = null) : base(errorCode, message, messageCode, metadata)
+        {
+        }
+    }
+}
