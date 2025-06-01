d50d5bd5aadb35982b11785c1a36f3a8ff792c85
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 17:41:44 +0200

    add execution redirection result: temporary redirection

---
+++ src/Cayd.AspNetCore.ExecutionResult/Redirection/ExecTemporaryRedirection.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.Redirection
+{
+    /// <summary>
+    /// Defines a redirection execution result with the status code of 307.
+    /// </summary>
+    public class ExecTemporaryRedirection : ExecRedirection
+    {
+        private const int redirectionCode = (int)HttpStatusCode.TemporaryRedirect;
+
+        /// <summary>
+        /// Creates a redirection execution result with the status code of 307.
+        /// </summary>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecTemporaryRedirection(object? metadata = null) : base(redirectionCode, metadata)
+        {
+        }
+    }
+}
