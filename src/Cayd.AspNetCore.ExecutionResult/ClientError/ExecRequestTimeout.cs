3790f51e2fd2d375502182c56713e80c11d6afb9
c-ayd <201085822+c-ayd@users.noreply.github.com>
2025-06-01 18:02:31 +0200

    add execution client error result: request timeout

---
+++ src/Cayd.AspNetCore.ExecutionResult/ClientError/ExecRequestTimeout.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace Cayd.AspNetCore.ExecutionResult.ClientError
+{
+    /// <summary>
+    /// Defines a failed execution result with the status code of 408.
+    /// </summary>
+    public class ExecRequestTimeout : ExecError
+    {
+        private const int errorCode = (int)HttpStatusCode.RequestTimeout;
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 408.
+        /// </summary>
+        /// <param name="details">The details of the errors of the exeucution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecRequestTimeout(ICollection<ExecErrorDetail> details, object? metadata = null) : base(errorCode, details, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 408.
+        /// </summary>
+        /// <param name="detail">The detail of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecRequestTimeout(ExecErrorDetail detail, object? metadata = null) : base(errorCode, detail, metadata)
+        {
+        }
+
+        /// <summary>
+        /// Creates a failed execution result with the status code of 408.
+        /// </summary>
+        /// <param name="message">The message of the error of the execution.</param>
+        /// <param name="messageCode">The message code of the error of the execution.</param>
+        /// <param name="metadata">Extra metadata of the execution.</param>
+        public ExecRequestTimeout(string? message = null, string? messageCode = null, object? metadata = null) : base(errorCode, message, messageCode, metadata)
+        {
+        }
+    }
+}
